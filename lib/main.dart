e75ef3435c43063ebaea2eed54dfbf03ca7680fd
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-09-07 23:08:42 +0300

    finish lig in page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/pages/login_%5Bage.dart';
+import 'package:chat_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,92 @@
+import 'package:chat_app/widgets/custom_elevation_button.dart';
+import 'package:chat_app/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff2b475e),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              const Spacer(
+                flex: 1,
+              ),
+              Image.asset('assets/images/scholar.png'),
+              const Text(
+                'Scholar Chat',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              const Spacer(
+                flex: 2,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      // fontSize: 1,
+                      fontFamily: 'Pacifico',
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 10,
+              ),
+              CustomTextField(
+                hientText: 'Email',
+              ),
+              Container(
+                height: 10,
+              ),
+              CustomTextField(
+                hientText: 'Password',
+              ),
+              Container(
+                height: 20,
+              ),
+              const CustomElevationButton(buttonText: 'Sign In'),
+              Container(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'You don\'t have acount?',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      ' Register.',
+                      style: TextStyle(
+                        color: Color(0xffC7EDE6),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(
+                flex: 2,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
