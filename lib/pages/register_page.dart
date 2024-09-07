01cb717e3dd00eeda022d57cd15010adaf76bf99
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-09-07 23:18:06 +0300

    create register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,10 +1,9 @@
-import 'package:chat_app/pages/register_page.dart';
 import 'package:chat_app/widgets/custom_elevation_button.dart';
 import 'package:chat_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +33,7 @@ class LoginPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
-                    'LOGIN',
+                    'REGISTER',
                     style: TextStyle(
                       color: Colors.white,
                       // fontSize: 1,
@@ -65,21 +64,15 @@ class LoginPage extends StatelessWidget {
               Row(
                 children: [
                   const Text(
-                    'You don\'t have acount?',
+                    'Already have an account?',
                     style: TextStyle(
                       color: Colors.white,
                     ),
                   ),
                   TextButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterPage(),
-                        ),
-                      );
-                    },
+                    onPressed: () {},
                     child: const Text(
-                      ' Register.',
+                      ' Login.',
                       style: TextStyle(
                         color: Color(0xffC7EDE6),
                       ),
