8475be7bd945aa784cbcfd52d33e4df8f9c98bb9
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-01-25 12:04:22 +0200

    add login logic in onMethod =>event handler

--- lib/pages/blocs/auth_bloc/auth_bloc.dart
+++ lib/pages/blocs/auth_bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'auth_event.dart';
@@ -6,8 +7,25 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+    on<AuthEvent>((event, emit) async {
+      if (event is LoginEvent) {
+        emit(RegisterLoading());
+        try {
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+              email: event.email, password: event.password);
+          emit(RegisterSuccess());
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'weak-password') {
+            emit(RegisterFailure(
+                errMessage: 'The password provided is too weak.'));
+          } else if (e.code == 'email-already-in-use') {
+            emit(RegisterFailure(
+                errMessage: 'The email already exists for that email.'));
+          }
+        } catch (e) {
+          emit(RegisterFailure(errMessage: e.toString()));
+        }
+      }
     });
   }
 }
